ea023e0012284523028b74e17bd443a590ce85a5
jbasilico <jbasilico@cognitivefoundry.org>
2011-11-27 00:09:06 -0800

    Added class for calculating divergences and putting them in a vector.

---
+++ Components/LearningCore/Source/gov/sandia/cognition/learning/function/distance/DivergencesEvaluator.java
@@ -0,0 +1,363 @@
+/*
+ * File:            ClusterDistanceEvaluator.java
+ * Authors:         Justin Basilico
+ * Project:         Cognitive Foundry Learning Core
+ * 
+ * Copyright 2011 Justin Basilico. All rights reserved.
+ */
+
+package gov.sandia.cognition.learning.function.distance;
+
+import gov.sandia.cognition.learning.algorithm.AbstractBatchLearnerContainer;
+import gov.sandia.cognition.learning.algorithm.BatchLearner;
+import gov.sandia.cognition.math.DivergenceFunction;
+import gov.sandia.cognition.math.matrix.DefaultVectorFactoryContainer;
+import gov.sandia.cognition.math.matrix.Vector;
+import gov.sandia.cognition.math.matrix.VectorFactory;
+import gov.sandia.cognition.math.matrix.VectorFactoryContainer;
+import gov.sandia.cognition.math.matrix.VectorOutputEvaluator;
+import gov.sandia.cognition.util.ObjectUtil;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Evaluates the divergence (distance) between an input and a list of values,
+ * storing the resulting divergence values in a vector. This can be used as a
+ * feature representation built from something like a clustering algorithm or
+ * from a set of prototype/basis elements.
+ *
+ * @param   <InputType>
+ *      The type of input value that the class evaluates. It is the second
+ *      parameter passed to the divergence function. Typically a type like
+ *      a Vector.
+ * @param   <ValueType>
+ *      The type of value that the divergence is computed from. It is the
+ *      first parameter passed to the divergence function. It is typically a
+ *      type like Vector or CenteroidCluster.
+ * @author  Justin Basilico
+ * @version 3.4.0
+ */
+public class DivergencesEvaluator<InputType, ValueType>
+    extends DefaultVectorFactoryContainer
+    implements VectorOutputEvaluator<InputType, Vector>,
+        DivergenceFunctionContainer<ValueType, InputType>
+{
+
+    /** The divergence function to apply between the data and the input. */
+    protected DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction;
+
+    /** The data to evaluate the divergence from. */
+    protected Collection<ValueType> values;
+
+    /**
+     * Creates a new {@code DivergencesEvaluator} with a null divergence
+     * function and an empty set of values.
+     */
+    public DivergencesEvaluator()
+    {
+        this(null, new ArrayList<ValueType>());
+    }
+
+    /**
+     * Creates a new {@code DivergencesEvaluator} with the given divergence 
+     * and values.
+     *
+     * @param   divergenceFunction
+     *      The divergence function to use.
+     * @param   values
+     *      The values to calculate the divergence from.
+     */
+    public DivergencesEvaluator(
+        final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction,
+        final Collection<ValueType> values)
+    {
+        this(divergenceFunction, values, VectorFactory.getDefault());
+    }
+
+    /**
+     * Creates a new {@code DivergencesEvaluator} with the given divergence
+     * and values.
+     *
+     * @param   divergenceFunction
+     *      The divergence function to use.
+     * @param   values
+     *      The values to calculate the divergence from.
+     * @param   vectorFactory
+     *      The vector factory to use.
+     */
+    public DivergencesEvaluator(
+        final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction,
+        final Collection<ValueType> values,
+        final VectorFactory<?> vectorFactory)
+    {
+        super(vectorFactory);
+
+        this.setDivergenceFunction(divergenceFunction);
+        this.setValues(values);
+    }
+
+    @Override
+    public DivergencesEvaluator<InputType, ValueType> clone()
+    {
+        @SuppressWarnings("unchecked")
+        final DivergencesEvaluator<InputType, ValueType> clone = (DivergencesEvaluator<InputType, ValueType>)
+            super.clone();
+        clone.divergenceFunction = ObjectUtil.cloneSafe(this.divergenceFunction);
+        clone.values = ObjectUtil.cloneSmartElementsAsArrayList(this.values);
+        return clone;
+    }
+
+    @Override
+    public Vector evaluate(
+        final InputType input)
+    {
+        // Create the result vector.
+        final int dimensionality = this.getOutputDimensionality();
+        final Vector result = this.getVectorFactory().createVector(
+            dimensionality);
+
+        // Go through the values and compute the divergence to each one.
+        int index = 0;
+        for (ValueType cluster : this.getValues())
+        {
+            final double distance =
+                this.divergenceFunction.evaluate(cluster, input);
+            result.setElement(index, distance);
+            index++;
+        }
+
+        // Return the result.
+        return result;
+    }
+
+    @Override
+    public int getOutputDimensionality()
+    {
+        return this.getValues().size();
+    }
+
+    @Override
+    public DivergenceFunction<? super ValueType, ? super InputType> getDivergenceFunction()
+    {
+        return this.divergenceFunction;
+    }
+
+    /**
+     * Sets the divergence function to use from the values to the inputs.
+     *
+     * @param   divergenceFunction
+     *      The divergence function to use.
+     */
+    public void setDivergenceFunction(
+        final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction)
+    {
+        this.divergenceFunction = divergenceFunction;
+    }
+
+    /**
+     * Gets the values that the divergence is computed from using the
+     * divergence function to the input.
+     * 
+     * @return
+     *      The values that the distance is computed from.
+     */
+    public Collection<ValueType> getValues()
+    {
+        return this.values;
+    }
+
+    /**
+     * Sets the values that the divergence is computed from using the
+     * divergence function to the input.
+     *
+     * @param   values
+     *      The values that the distance is computed from.
+     */
+    public void setValues(
+        final Collection<ValueType> values)
+    {
+        this.values = values;
+    }
+
+    /**
+     * Convinience method for creation a {@code DivergeceEvaluator}.
+     *
+     * @param   <InputType>
+     *      The type of input value that the class evaluates.
+     * @param   <ValueType>
+     *      The type of value that the divergence is computed from.
+     * @param   divergenceFunction
+     *      The divergence function to use.
+     * @param   values
+     *      The values to calculate the divergence from.
+     * @return
+     *      A new evaluator.
+     */
+    public static <InputType, ValueType> DivergencesEvaluator<InputType, ValueType>
+        create(
+        final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction,
+        final Collection<ValueType> values)
+    {
+        return new DivergencesEvaluator<InputType, ValueType>(
+            divergenceFunction, values);
+    }
+
+    /**
+     * A learner adapter for the {@code DivergencesEvaluator}. It calls a
+     * base learner and then wraps learned collection of values in an evaluator
+     * that uses the given divergence function.
+     *
+     * @param   <DataType>
+     *      The data type for learning. Passed to the wrapped learner.
+     * @param   <InputType>
+     *      The input type for the evaluator.
+     * @param   <ValueType>
+     *      The value type that is the output of learning and is used as the
+     *      values in the learned evaluator.
+     */
+    public static class Learner<DataType, InputType, ValueType>
+        extends AbstractBatchLearnerContainer<BatchLearner<? super DataType, ? extends Collection<ValueType>>>
+        implements BatchLearner<DataType, DivergencesEvaluator<InputType, ValueType>>,
+            DivergenceFunctionContainer<ValueType, InputType>,
+            VectorFactoryContainer
+    {
+
+        /** The divergence function to apply between the data and the input. */
+        protected DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction;
+
+        /** The vector factory to use. */
+        protected VectorFactory<?> vectorFactory;
+
+        /**
+         * Creates a new {@code DivergenceFunction.Learner} with null base
+         * learner and divergence functions.
+         */
+        public Learner()
+        {
+            this(null, null);
+        }
+
+        /**
+         * Creates a new {@code DivergenceFunction.Learner} with the given
+         * properties.
+         *
+         * @param   learner
+         *      The base learner to use.
+         * @param   divergenceFunction
+         *      The divergence function to use.
+         */
+        public Learner(
+            final BatchLearner<DataType, ? extends Collection<ValueType>> learner,
+            final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction)
+        {
+            this(learner, divergenceFunction, VectorFactory.getDefault());
+        }
+
+        /**
+         * Creates a new {@code DivergenceFunction.Learner} with the given
+         * properties.
+         *
+         * @param   learner
+         *      The base learner to use.
+         * @param   divergenceFunction
+         *      The divergence function to use.
+         * @param   vectorFactory
+         *      The vector factory to use.
+         */
+        public Learner(
+            final BatchLearner<DataType, ? extends Collection<ValueType>> learner,
+            final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction,
+            final VectorFactory<?> vectorFactory)
+        {
+            super(learner);
+
+            this.setDivergenceFunction(divergenceFunction);
+            this.setVectorFactory(vectorFactory);
+        }
+
+        @Override
+        public Learner<DataType, InputType, ValueType> clone()
+        {
+            @SuppressWarnings("unchecked")
+            final Learner<DataType, InputType, ValueType> clone = (Learner<DataType, InputType, ValueType>)
+                super.clone();
+            clone.divergenceFunction = ObjectUtil.cloneSafe(this.divergenceFunction);
+            return clone;
+        }
+        
+        @Override
+        public DivergencesEvaluator<InputType, ValueType> learn(
+            final DataType data)
+        {
+            return new DivergencesEvaluator<InputType, ValueType>(
+                this.getDivergenceFunction(), 
+                this.getLearner().learn(data),
+                this.getVectorFactory());
+        }
+
+        @Override
+        public DivergenceFunction<? super ValueType, ? super InputType> getDivergenceFunction()
+        {
+            return this.divergenceFunction;
+        }
+
+        /**
+         * Sets the divergence function to use from the values to the inputs.
+         *
+         * @param   divergenceFunction
+         *      The divergence function to use.
+         */
+        public void setDivergenceFunction(
+            final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction)
+        {
+            this.divergenceFunction = divergenceFunction;
+        }
+
+        @Override
+        public VectorFactory<? extends Vector> getVectorFactory()
+        {
+            return this.vectorFactory;
+        }
+
+        /**
+         * Sets the vector factory to use.
+         *
+         * @param   vectorFactory
+         *      The vector factory to use.
+         */
+        public void setVectorFactory(
+            final VectorFactory<?> vectorFactory)
+        {
+            this.vectorFactory = vectorFactory;
+        }
+
+        /**
+         * Convenience method for creating a
+         * {@code DivergencesEvaluator.Learner}.
+         *
+         * @param   <DataType>
+         *      The data type for learning. Passed to the wrapped learner.
+         * @param   <InputType>
+         *      The input type for the evaluator.
+         * @param   <ValueType>
+         *      The value type that is the output of learning and is used as the
+         *      values in the learned evaluator.
+         * @param   learner
+         *      The base learner to use.
+         * @param   divergenceFunction
+         *      The divergence function to use.
+         * @return
+         *      A new learner.
+         */
+        public static <DataType, InputType, ValueType> Learner<DataType, InputType, ValueType>
+            create(
+            final BatchLearner<DataType, ? extends Collection<ValueType>> learner,
+            final DivergenceFunction<? super ValueType, ? super InputType> divergenceFunction)
+        {
+            return new Learner<DataType, InputType, ValueType>(
+                learner, divergenceFunction);
+        }
+
+    }
+
+}
---
+++ Components/LearningCore/Test/gov/sandia/cognition/learning/function/distance/DivergencesEvaluatorTest.java
@@ -0,0 +1,232 @@
+/*
+ * File:            DivergencesEvaluatorTest.java
+ * Authors:         Justin Basilico
+ * Project:         Cognitive Foundry Learning Core
+ * 
+ * Copyright 2011 Justin Basilico. All rights reserved.
+ */
+
+package gov.sandia.cognition.learning.function.distance;
+
+import org.junit.Test;
+import gov.sandia.cognition.learning.algorithm.clustering.KMeansFactory;
+import gov.sandia.cognition.learning.algorithm.clustering.cluster.CentroidCluster;
+import gov.sandia.cognition.math.DivergenceFunction;
+import gov.sandia.cognition.math.matrix.Vector;
+import gov.sandia.cognition.math.matrix.VectorFactory;
+import gov.sandia.cognition.math.matrix.Vectorizable;
+import gov.sandia.cognition.math.matrix.mtj.SparseVectorFactoryMTJ;
+import gov.sandia.cognition.math.matrix.mtj.Vector2;
+import gov.sandia.cognition.math.matrix.mtj.Vector3;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for class DivergencesEvaluator.
+ * 
+ * @author  Justin Basilico
+ * @since   3.4.0
+ */
+public class DivergencesEvaluatorTest
+{
+    /** Random number generator to use. */
+    protected Random random = new Random(211);
+
+    /**
+     * Creates a new test.
+     */
+    public DivergencesEvaluatorTest()
+    {
+    }
+
+    /**
+     * Test of constructors of class DivergencesEvaluator.
+     */
+    @Test
+    public void testConstructors()
+    {
+        DivergenceFunction<Vectorizable, Vectorizable> divergenceFunction = null;
+        Collection<Vector> values = null;
+        VectorFactory<?> vectorFactory = VectorFactory.getDefault();
+        DivergencesEvaluator<Vector, Vector> instance = new DivergencesEvaluator<Vector, Vector>();
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+        assertTrue(instance.getValues().isEmpty());
+        assertSame(vectorFactory, instance.getVectorFactory());
+
+        divergenceFunction = new ManhattanDistanceMetric();
+        values = new ArrayList<Vector>();
+        values.add(new Vector2());
+        instance = new DivergencesEvaluator<Vector, Vector>(
+            divergenceFunction, values);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+        assertSame(values, instance.getValues());
+        assertSame(vectorFactory, instance.getVectorFactory());
+
+        vectorFactory = new SparseVectorFactoryMTJ();
+        instance = new DivergencesEvaluator<Vector, Vector>(
+            divergenceFunction, values, vectorFactory);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+        assertSame(values, instance.getValues());
+        assertSame(vectorFactory, instance.getVectorFactory());
+    }
+
+    /**
+     * Test of clone method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testClone()
+    {
+        DivergencesEvaluator<Vector, Vector> instance = new DivergencesEvaluator<Vector, Vector>();
+        DivergencesEvaluator<Vector, Vector> clone = instance.clone();
+        assertNotSame(clone, instance);
+        assertNotSame(clone, instance.clone());
+    }
+
+    /**
+     * Test of evaluate method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testEvaluate()
+    {
+        DivergencesEvaluator<Vector, Vector3> instance =
+            DivergencesEvaluator.create(new ManhattanDistanceMetric(),
+                Arrays.asList(
+                    new Vector3( 1.0,  0.0,  0.0),
+                    new Vector3( 0.0, -3.0,  0.0),
+                    new Vector3( 0.0,  0.0,  2.0),
+                    new Vector3( 3.0,  2.0,  1.0)));
+
+        VectorFactory<?> vectorFactory = VectorFactory.getDenseDefault();
+        assertEquals(vectorFactory.copyValues(2.0, 4.0, 3.0, 5.0),
+            instance.evaluate(new Vector3(1.0, -1.0, 1.0)));
+        assertEquals(vectorFactory.copyValues(4.0, 0.0, 5.0, 9.0),
+            instance.evaluate(new Vector3(0.0, -3.0, 0.0)));
+    }
+
+    /**
+     * Test of getOutputDimensionality method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testGetOutputDimensionality()
+    {
+        DivergencesEvaluator<Vector, Vector> instance =
+            new DivergencesEvaluator<Vector, Vector>();
+        assertEquals(0, instance.getOutputDimensionality());
+
+        instance.getValues().add(new Vector3());
+        assertEquals(1, instance.getOutputDimensionality());
+
+        instance.getValues().add(new Vector3());
+        assertEquals(2, instance.getOutputDimensionality());
+
+        instance.setValues(new LinkedHashSet<Vector>());
+        assertEquals(0, instance.getOutputDimensionality());
+    }
+
+    /**
+     * Test of getDivergenceFunction method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testGetDivergenceFunction()
+    {
+        this.testSetDivergenceFunction();
+    }
+
+    /**
+     * Test of setDivergenceFunction method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testSetDivergenceFunction()
+    {
+        DivergenceFunction<Vectorizable, Vectorizable> divergenceFunction = null;
+        DivergencesEvaluator<Vector, Vector> instance = new DivergencesEvaluator<Vector, Vector>();
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+
+        divergenceFunction = new ManhattanDistanceMetric();
+        instance.setDivergenceFunction(divergenceFunction);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+
+        divergenceFunction = null;
+        instance.setDivergenceFunction(divergenceFunction);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+
+        divergenceFunction = new EuclideanDistanceSquaredMetric();
+        instance.setDivergenceFunction(divergenceFunction);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+    }
+
+    /**
+     * Test of getValues method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testGetValues()
+    {
+        this.testSetValues();;
+    }
+
+    /**
+     * Test of setValues method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testSetValues()
+    {
+        Collection<Vector> values = null;
+        DivergencesEvaluator<Vector, Vector> instance = new DivergencesEvaluator<Vector, Vector>();
+        assertTrue(instance.getValues().isEmpty());
+
+        values = new ArrayList<Vector>();
+        values.add(new Vector2());
+        instance.setValues(values);
+        assertSame(values, instance.getValues());
+
+        values = new HashSet<Vector>();
+        instance.setValues(values);
+        assertSame(values, instance.getValues());
+    }
+
+    /**
+     * Test of create method, of class DivergencesEvaluator.
+     */
+    @Test
+    public void testCreate()
+    {
+        ManhattanDistanceMetric divergenceFunction =
+            new ManhattanDistanceMetric();
+        Collection<Vector> values = new ArrayList<Vector>();
+        values.add(new Vector2());
+        DivergencesEvaluator<?, Vector> instance =
+            DivergencesEvaluator.create(divergenceFunction, values);
+        assertSame(divergenceFunction, instance.getDivergenceFunction());
+        assertSame(values, instance.getValues());
+    }
+
+    /**
+     * Test of learn method, of class DivergencesEvaluator.Learner.
+     */
+    @Test
+    public void testLearn()
+    {
+        VectorFactory<?> vectorFactory = VectorFactory.getDenseDefault();
+        DivergencesEvaluator.Learner<Collection<? extends Vector>, Vector, CentroidCluster<Vector>> instance =
+            DivergencesEvaluator.Learner.create(
+                KMeansFactory.create(2, random),
+                KMeansFactory.create(2, random).getDivergenceFunction());
+
+        Collection<Vector> data = new ArrayList<Vector>();
+        data.add(vectorFactory.copyValues( 0.0,  1.0,  0.0,  0.0));
+        data.add(vectorFactory.copyValues( 0.0, -1.0,  0.0,  0.0));
+        data.add(vectorFactory.copyValues( 0.0,  0.0,  3.0,  0.0));
+        data.add(vectorFactory.copyValues( 0.0,  0.0,  5.0,  0.0));
+
+        DivergencesEvaluator<Vector, ?> learned = instance.learn(data);
+        assertEquals(new Vector2(4.0, 0.0), 
+            learned.evaluate(vectorFactory.copyValues(0.0, 0.0, 0.0, 0.0)));
+        assertEquals(new Vector2(0.0, 4.0),
+            learned.evaluate(vectorFactory.copyValues(0.0, 0.0, 4.0, 0.0)));
+    }
+}
