c396f99bd78d7e7ffc9fc18228307cf264583e8f
jbasilico <jbasilico@cognitivefoundry.org>
2012-01-04 21:56:21 -0800

    Added support for random subspace method.

---
+++ Components/LearningCore/Source/gov/sandia/cognition/learning/data/feature/RandomSubspace.java
@@ -0,0 +1,188 @@
+/*
+ * File:            RandomSubspace.java
+ * Authors:         Justin Basilico
+ * Project:         Community Foundry
+ * 
+ * Copyright 2011 Justin Basilico. All rights reserved.
+ */
+
+package gov.sandia.cognition.learning.data.feature;
+
+import gov.sandia.cognition.annotation.PublicationReference;
+import gov.sandia.cognition.annotation.PublicationType;
+import gov.sandia.cognition.learning.algorithm.BatchLearner;
+import gov.sandia.cognition.learning.data.DatasetUtil;
+import gov.sandia.cognition.learning.function.vector.SubVectorEvaluator;
+import gov.sandia.cognition.math.Permutation;
+import gov.sandia.cognition.math.matrix.Vector;
+import gov.sandia.cognition.math.matrix.VectorFactory;
+import gov.sandia.cognition.math.matrix.VectorFactoryContainer;
+import gov.sandia.cognition.math.matrix.Vectorizable;
+import gov.sandia.cognition.util.AbstractRandomized;
+import gov.sandia.cognition.util.ArgumentChecker;
+import gov.sandia.cognition.util.Randomized;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+
+/**
+ * Selects a random subspace from the given vector, which is a random set of
+ * indices. It is typically used in the context of ensemble learning to adapt
+ * a base learner to increase the variance, similar to the method used in
+ * bagging or random forests. Thus, it is also known as attribute bagging.
+ *
+ * @author  Justin Basilico
+ * @version 3.4.0
+ */
+@PublicationReference(
+    title="Random Subspace Method",
+    author="Wikipedia",
+    year=2011,
+    type=PublicationType.WebPage,
+    url="http://en.wikipedia.org/wiki/Random_subspace_method")
+public class RandomSubspace
+    extends AbstractRandomized
+    implements BatchLearner<Collection<? extends Vectorizable>, SubVectorEvaluator>,
+        Randomized, VectorFactoryContainer
+{
+    /** The default size is {@value}. */
+    public static final int DEFAULT_SIZE = 10;
+
+    /** The size of the random subspace to create, which is the number of
+     *  dimensions that are chosen. */
+    protected int size;
+
+    /** The vector factory for the sub vector evaluator to use. */
+    protected VectorFactory<?> vectorFactory;
+
+    /**
+     * Creates a new {@code RandomSubspace} with the default size.
+     */
+    public RandomSubspace()
+    {
+        this(DEFAULT_SIZE);
+    }
+
+    /**
+     * Creates a new {@code RandomSubspace} with the given size.
+     *
+     * @param   size
+     *      The size of the subspace to create. Must be positive.
+     */
+    public RandomSubspace(
+        final int size)
+    {
+        this(size, new Random());
+    }
+
+    /**
+     * Creates a new {@code RandomSubspace} with the given parameters.
+     *
+     * @param   size
+     *      The size of the subspace to create. Must be positive.
+     * @param   random
+     *      The random number generator to use.
+     */
+    public RandomSubspace(
+        final int size,
+        final Random random)
+    {
+        this(size, random, VectorFactory.getDefault());
+    }
+
+    /**
+     * Creates a new {@code RandomSubspace} with the given parameters.
+     *
+     * @param   size
+     *      The size of the subspace to create. Must be positive.
+     * @param   random
+     *      The random number generator to use.
+     * @param   vectorFactory
+     *      The vector factory to use in the result to create
+     */
+    public RandomSubspace(
+        final int size,
+        final Random random,
+        final VectorFactory<?> vectorFactory)
+    {
+        super(random);
+
+        this.setSize(size);
+        this.setVectorFactory(vectorFactory);
+    }
+
+    @Override
+    public SubVectorEvaluator learn(
+        final Collection<? extends Vectorizable> data)
+    {
+        // Figure out the dimensionality of the data.
+        final int dimensionality = DatasetUtil.getDimensionality(data);
+
+        // Can't learn with a bad dimensionality.
+        if (dimensionality < 0)
+        {
+            return null;
+        }
+
+// TODO: If we had a method for creating a partial permutation, we could avoid
+// this code. -- jbasilico (2011-10-18)
+
+        // Sample the indices we want to use without replacement.
+        // We do this by creating a permutation and then only taking the first
+        // part of it, containing the indices we will use.
+        final int[] permutation = Permutation.createPermutation(
+            dimensionality, this.getRandom());
+        final int[] indices = new int[Math.min(size, dimensionality)];
+        System.arraycopy(permutation, 0, indices, 0, indices.length);
+
+        // We sort the indices just to make their interpretation easier.
+        Arrays.sort(indices);
+
+        // Create the result.
+        return new SubVectorEvaluator(dimensionality, indices,
+            this.getVectorFactory());
+    }
+
+    /**
+     * Gets the size of the subspace that will be created.
+     *
+     * @return
+     *      The size of the subspace that will be created. Must be positive.
+     */
+    public int getSize()
+    {
+        return this.size;
+    }
+
+    /**
+     * Sets the size of the subspace that will be created.
+     *
+     * @param   size
+     *      The size of the subspace that will be created. Must be positive.
+     */
+    public void setSize(
+        final int size)
+    {
+        ArgumentChecker.assertIsPositive("size", size);
+        this.size = size;
+    }
+
+    @Override
+    public VectorFactory<? extends Vector> getVectorFactory()
+    {
+        return this.vectorFactory;
+    }
+
+    /**
+     * Sets the vector factory to use.
+     *
+     * @param   vectorFactory
+     *      The vector factory to use.
+     */
+    public void setVectorFactory(
+        final VectorFactory<?> vectorFactory)
+    {
+        this.vectorFactory = vectorFactory;
+    }
+
+}
---
+++ Components/LearningCore/Test/gov/sandia/cognition/learning/data/feature/RandomSubspaceTest.java
@@ -0,0 +1,194 @@
+/*
+ * File:            RandomSubspaceTest.java
+ * Authors:         Justin Basilico
+ * Project:         Community Foundry
+ * 
+ * Copyright 2011 Justin Basilico. All rights reserved.
+ */
+
+package gov.sandia.cognition.learning.data.feature;
+
+import gov.sandia.cognition.math.matrix.Vector;
+import gov.sandia.cognition.learning.function.vector.SubVectorEvaluator;
+import gov.sandia.cognition.math.matrix.VectorFactory;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Set;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for class RandomSubspace.
+ *
+ * @author  Justin Basilico
+ * @since   3.4.0
+ */
+public class RandomSubspaceTest
+{
+
+    /** Random number generator. */
+    protected Random random = new Random(4777);
+
+    /**
+     * Creates a new test.
+     */
+    public RandomSubspaceTest()
+    {
+        super();
+    }
+
+    /**
+     * Test of constructors method, of class RandomSubspace.
+     */
+    @Test
+    public void testConstructors()
+    {
+        int size = RandomSubspace.DEFAULT_SIZE;
+        VectorFactory<?> vectorFactory = VectorFactory.getDefault();
+        RandomSubspace instance = new RandomSubspace();
+        assertEquals(size, instance.getSize());
+        assertNotNull(instance.getRandom());
+        assertSame(vectorFactory, instance.getVectorFactory());
+        
+        size = this.random.nextInt(1000) + 1;
+        instance = new RandomSubspace(size);
+        assertNotNull(instance.getRandom());
+        assertSame(vectorFactory, instance.getVectorFactory());
+        assertNotNull(instance.getRandom());
+
+        Random random = new Random();
+        instance = new RandomSubspace(size, random);
+        assertNotNull(instance.getRandom());
+        assertSame(vectorFactory, instance.getVectorFactory());
+        assertNotNull(instance.getRandom());
+
+        vectorFactory = VectorFactory.getSparseDefault();
+        instance = new RandomSubspace(size, random, vectorFactory);
+        assertNotNull(instance.getRandom());
+        assertSame(vectorFactory, instance.getVectorFactory());
+        assertNotNull(instance.getRandom());
+    }
+
+    /**
+     * Test of learn method, of class RandomSubspace.
+     */
+    @Test
+    public void testLearn()
+    {
+        int size = 3;
+        RandomSubspace instance = new RandomSubspace(size, random);
+        assertNull(instance.learn(null));
+        assertNull(instance.learn(new LinkedList<Vector>()));
+
+        int dimensionality = 7;
+        Collection<Vector> data = new ArrayList<Vector>();
+        data.add(VectorFactory.getDefault().createVector(dimensionality));
+        data.add(VectorFactory.getDefault().createVector(dimensionality));
+
+        SubVectorEvaluator result = instance.learn(data);
+        assertEquals(dimensionality, result.getInputDimensionality());
+        assertEquals(size, result.getOutputDimensionality());
+
+        Set<Integer> indexSet = new LinkedHashSet<Integer>();
+        for (int index : result.getSubIndices())
+        {
+            assertTrue(index >= 0);
+            assertTrue(index < dimensionality);
+            indexSet.add(index);
+        }
+        assertEquals(size, indexSet.size());
+
+        size = dimensionality * 2;
+        instance.setSize(size);
+        result = instance.learn(data);
+        size = dimensionality;
+        assertEquals(dimensionality, result.getInputDimensionality());
+        assertEquals(size, result.getOutputDimensionality());
+        indexSet = new LinkedHashSet<Integer>();
+        for (int index : result.getSubIndices())
+        {
+            assertTrue(index >= 0);
+            assertTrue(index < dimensionality);
+            indexSet.add(index);
+        }
+        assertEquals(size, indexSet.size());
+    }
+
+    /**
+     * Test of getSize method, of class RandomSubspace.
+     */
+    @Test
+    public void testGetSize()
+    {
+        this.testSetSize();
+    }
+
+    /**
+     * Test of setSize method, of class RandomSubspace.
+     */
+    @Test
+    public void testSetSize()
+    {
+        int size = RandomSubspace.DEFAULT_SIZE;
+        RandomSubspace instance = new RandomSubspace();
+        assertEquals(size, instance.getSize());
+
+        int[] goodValues = {1, 2, 5, 1000, 4, random.nextInt(100) + 1};
+        for (int goodValue : goodValues)
+        {
+            size = goodValue;
+            instance.setSize(size);
+        }
+
+        int[] badValues = {0, -1, -2, -45, -random.nextInt(100)};
+        for (int badValue : badValues)
+        {
+            boolean exceptionThrown = false;
+            try
+            {
+                instance.setSize(badValue);
+            }
+            catch (IllegalArgumentException e)
+            {
+                exceptionThrown = true;
+            }
+            finally
+            {
+                assertTrue(exceptionThrown);
+            }
+            assertEquals(size, instance.getSize());
+        }
+    }
+
+    /**
+     * Test of getVectorFactory method, of class RandomSubspace.
+     */
+    @Test
+    public void testGetVectorFactory()
+    {
+        this.testSetVectorFactory();
+    }
+
+    /**
+     * Test of setVectorFactory method, of class RandomSubspace.
+     */
+    @Test
+    public void testSetVectorFactory()
+    {
+        VectorFactory<?> vectorFactory = VectorFactory.getDefault();
+        RandomSubspace instance = new RandomSubspace();
+        assertSame(vectorFactory, instance.getVectorFactory());
+
+        vectorFactory = VectorFactory.getSparseDefault();
+        instance.setVectorFactory(vectorFactory);
+        assertSame(vectorFactory, instance.getVectorFactory());
+        
+        vectorFactory = VectorFactory.getDenseDefault();
+        instance.setVectorFactory(vectorFactory);
+        assertSame(vectorFactory, instance.getVectorFactory());
+    }
+
+}
