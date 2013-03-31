1dcf32f2f18db9f81778874d53d62495d3ae9bf7
jbasilico <jbasilico@cognitivefoundry.org>
2013-03-30 23:07:31 -0700

    Updated utility with more methods for other common types of arrays.

--- Components/CommonCore/Source/gov/sandia/cognition/collection/ArrayUtil.java
+++ Components/CommonCore/Source/gov/sandia/cognition/collection/ArrayUtil.java
@@ -34,8 +34,50 @@ public class ArrayUtil
      * @return
      *      A copy of the given array.
      */
+    public static boolean[] copy(
+        final boolean[] array)
+    {
+        if (array == null)
+        {
+            return null;
+        }
+        else
+        {
+            return Arrays.copyOf(array, array.length);
+        }
+    }
+
+    /**
+     * Creates a new copy of the given array.
+     *
+     * @param   array
+     *      The array to copy.
+     * @return
+     *      A copy of the given array.
+     */
     public static int[] copy(
         final int[] array)
+    {
+        if (array == null)
+        {
+            return null;
+        }
+        else
+        {
+            return Arrays.copyOf(array, array.length);
+        }
+    }
+
+    /**
+     * Creates a new copy of the given array.
+     *
+     * @param   array
+     *      The array to copy.
+     * @return
+     *      A copy of the given array.
+     */
+    public static long[] copy(
+        final long[] array)
     {
         if (array == null)
         {
@@ -65,6 +107,52 @@ public class ArrayUtil
         else
         {
             return Arrays.copyOf(array, array.length);
+        }
+    }
+
+    /**
+     * Creates a new copy of the given array. Does not copy the elements.
+     *
+     * @param   array
+     *      The array to copy. Does not copy the elements.
+     * @return
+     *      A copy of the given array.
+     */
+    public static <T> T[] copy(
+        final T[] array)
+    {
+        if (array == null)
+        {
+            return null;
+        }
+        else
+        {
+            return Arrays.copyOf(array, array.length);
+        }
+    }
+
+    /**
+     * Reverses the ordering of elements in an array.
+     *
+     * @param   array
+     *      The array to reverse the elements in.
+     */
+    public static void reverse(
+        final boolean[] array)
+    {
+        if (array != null)
+        {
+            final int length = array.length;
+            int i = 0;
+            int j = length - 1;
+            while (i < j)
+            {
+                final boolean temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
         }
     }
 
@@ -100,6 +188,31 @@ public class ArrayUtil
      *      The array to reverse the elements in.
      */
     public static void reverse(
+        final long[] array)
+    {
+        if (array != null)
+        {
+            final int length = array.length;
+            int i = 0;
+            int j = length - 1;
+            while (i < j)
+            {
+                final long temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+
+    /**
+     * Reverses the ordering of elements in an array.
+     *
+     * @param   array
+     *      The array to reverse the elements in.
+     */
+    public static void reverse(
         final double[] array)
     {
         if (array != null)
@@ -117,4 +230,100 @@ public class ArrayUtil
             }
         }
     }
+
+    /**
+     * Reverses the ordering of elements in an array.
+     *
+     * @param   array
+     *      The array to reverse the elements in.
+     */
+    public static void reverse(
+        final Object[] array)
+    {
+        if (array != null)
+        {
+            final int length = array.length;
+            int i = 0;
+            int j = length - 1;
+            while (i < j)
+            {
+                final Object temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+
+    /**
+     * Determines if the given array is null or empty (length 0).
+     *
+     * @param   array
+     *      The array.
+     * @return
+     *      True if the array is null or length 0. Otherwise, false.
+     */
+    public static boolean isEmpty(
+        final boolean[] array)
+    {
+        return array == null || array.length <= 0;
+    }
+
+    /**
+     * Determines if the given array is null or empty (length 0).
+     *
+     * @param   array
+     *      The array.
+     * @return
+     *      True if the array is null or length 0. Otherwise, false.
+     */
+    public static boolean isEmpty(
+        final int[] array)
+    {
+        return array == null || array.length <= 0;
+    }
+
+    /**
+     * Determines if the given array is null or empty (length 0).
+     *
+     * @param   array
+     *      The array.
+     * @return
+     *      True if the array is null or length 0. Otherwise, false.
+     */
+    public static boolean isEmpty(
+        final long[] array)
+    {
+        return array == null || array.length <= 0;
+    }
+    
+    /**
+     * Determines if the given array is null or empty (length 0).
+     *
+     * @param   array
+     *      The array.
+     * @return
+     *      True if the array is null or length 0. Otherwise, false.
+     */
+    public static boolean isEmpty(
+        final double[] array)
+    {
+        return array == null || array.length <= 0;
+    }
+    
+    /**
+     * Determines if the given array is null or empty (length 0).
+     *
+     * @param   array
+     *      The array.
+     * @return
+     *      True if the array is null or length 0. Otherwise, false.
+     */
+    public static boolean isEmpty(
+        final Object[] array)
+    {
+        return array == null || array.length <= 0;
+    }
+
 }
--- Components/CommonCore/Test/gov/sandia/cognition/collection/ArrayUtilTest.java
+++ Components/CommonCore/Test/gov/sandia/cognition/collection/ArrayUtilTest.java
@@ -40,6 +40,23 @@ public class ArrayUtilTest
     /**
      * Test of copy method, of class ArrayUtil.
      */
+    public void testCopyBooleanArray()
+    {
+        boolean[] array = null;
+        assertNull(ArrayUtil.copy(array));
+
+        array = new boolean[] { true, false, false };
+        boolean[] result = ArrayUtil.copy(array);
+        assertNotSame(array, result);
+        assertTrue(Arrays.equals(array, result));
+        assertEquals(true, array[0]);
+        assertEquals(false, array[1]);
+        assertEquals(false, array[2]);
+    }
+
+    /**
+     * Test of copy method, of class ArrayUtil.
+     */
     public void testCopyIntArray()
     {
         int[] array = null;
@@ -57,6 +74,23 @@ public class ArrayUtilTest
     /**
      * Test of copy method, of class ArrayUtil.
      */
+    public void testCopyLongArray()
+    {
+        long[] array = null;
+        assertNull(ArrayUtil.copy(array));
+
+        array = new long[] { 1, 0, Long.MAX_VALUE };
+        long[] result = ArrayUtil.copy(array);
+        assertNotSame(array, result);
+        assertTrue(Arrays.equals(array, result));
+        assertEquals(1, array[0]);
+        assertEquals(0, array[1]);
+        assertEquals(Long.MAX_VALUE, array[2]);
+    }
+
+    /**
+     * Test of copy method, of class ArrayUtil.
+     */
     public void testCopyDoubleArray()
     {
         double[] array = null;
@@ -69,6 +103,58 @@ public class ArrayUtilTest
         assertEquals(1, array[0], 0.0);
         assertEquals(0, array[1], 0.0);
         assertEquals(-0.5, array[2], 0.0);
+    }
+
+    /**
+     * Test of copy method, of class ArrayUtil.
+     */
+    public void testCopyObjectArray()
+    {
+        Object[] array = null;
+        assertNull(ArrayUtil.copy(array));
+
+        array = new Object[0];
+        Object[] result = ArrayUtil.copy(array);
+        assertNotSame(array, result);
+        assertTrue(Arrays.equals(array, result));
+
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Object o3 = null;
+        array = new Object[] { o1, o2, o3};
+        result = ArrayUtil.copy(array);
+        assertNotSame(array, result);
+        assertTrue(Arrays.equals(array, result));
+        assertSame(o1, array[0]);
+        assertSame(o2, array[1]);
+        assertSame(o3, array[2]);
+    }
+
+    /**
+     * Test of reverse method, of class ArrayUtil.
+     */
+    public void testReverseBooleanArray()
+    {
+        boolean[] array = null;
+        ArrayUtil.reverse(array);
+
+        array = new boolean[0];
+        ArrayUtil.reverse(array);
+
+        array = new boolean[] { true };
+        ArrayUtil.reverse(array);
+        assertEquals(true, array[0]);
+
+        array = new boolean[] { false, true };
+        ArrayUtil.reverse(array);
+        assertEquals(true, array[0]);
+        assertEquals(false, array[1]);
+
+        array = new boolean[] { false, false, true };
+        ArrayUtil.reverse(array);
+        assertEquals(true, array[0]);
+        assertEquals(false, array[1]);
+        assertEquals(false, array[2]);
     }
 
     /**
@@ -98,6 +184,32 @@ public class ArrayUtilTest
         assertEquals(1, array[2]);
     }
 
+    /**
+     * Test of reverse method, of class ArrayUtil.
+     */
+    public void testReverseLongArray()
+    {
+        long[] array = null;
+        ArrayUtil.reverse(array);
+
+        array = new long[0];
+        ArrayUtil.reverse(array);
+
+        array = new long[] { 9 };
+        ArrayUtil.reverse(array);
+        assertEquals(9, array[0]);
+
+        array = new long[] { 8, 1 };
+        ArrayUtil.reverse(array);
+        assertEquals(1, array[0]);
+        assertEquals(8, array[1]);
+
+        array = new long[] { 1, 0, Long.MAX_VALUE };
+        ArrayUtil.reverse(array);
+        assertEquals(Long.MAX_VALUE, array[0]);
+        assertEquals(0, array[1]);
+        assertEquals(1, array[2]);
+    }
 
     /**
      * Test of reverse method, of class ArrayUtil.
@@ -124,6 +236,96 @@ public class ArrayUtilTest
         assertEquals(5.0, array[0], 0.0);
         assertEquals(0.0, array[1], 0.0);
         assertEquals(1.0, array[2], 0.0);
+    }
+
+    /**
+     * Test of reverse method, of class ArrayUtil.
+     */
+    public void testReverseObjectArray()
+    {
+        Object[] array = null;
+        ArrayUtil.reverse(array);
+
+        array = new Object[0];
+        ArrayUtil.reverse(array);
+
+        Object o1 = new Object();
+        array = new Object[] { o1 };
+        ArrayUtil.reverse(array);
+        assertSame(o1, array[0]);
+
+        Object o2 = new Object();
+        array = new Object[] { o1, o2 };
+        ArrayUtil.reverse(array);
+        assertSame(o2, array[0]);
+        assertSame(o1, array[1]);
+
+        array = new Object[] { o1, o2, null};
+        ArrayUtil.reverse(array);
+        assertSame(null, array[0]);
+        assertSame(o2, array[1]);
+        assertSame(o1, array[2]);
+    }
+
+    /**
+     * Test of isEmpty method, of class ArrayUtil.
+     */
+    public void testIsEmptyBooleanArray()
+    {
+        assertTrue(ArrayUtil.isEmpty((boolean[]) null));
+        assertTrue(ArrayUtil.isEmpty(new boolean[0]));
+        assertFalse(ArrayUtil.isEmpty(new boolean[1]));
+        assertFalse(ArrayUtil.isEmpty(new boolean[2]));
+        assertFalse(ArrayUtil.isEmpty(new boolean[10]));
+    }
+
+    /**
+     * Test of isEmpty method, of class ArrayUtil.
+     */
+    public void testIsEmptyIntArray()
+    {
+        assertTrue(ArrayUtil.isEmpty((int[]) null));
+        assertTrue(ArrayUtil.isEmpty(new int[0]));
+        assertFalse(ArrayUtil.isEmpty(new int[1]));
+        assertFalse(ArrayUtil.isEmpty(new int[2]));
+        assertFalse(ArrayUtil.isEmpty(new int[10]));
+    }
+
+    /**
+     * Test of isEmpty method, of class ArrayUtil.
+     */
+    public void testIsEmptyLongArray()
+    {
+        assertTrue(ArrayUtil.isEmpty((long[]) null));
+        assertTrue(ArrayUtil.isEmpty(new long[0]));
+        assertFalse(ArrayUtil.isEmpty(new long[1]));
+        assertFalse(ArrayUtil.isEmpty(new long[2]));
+        assertFalse(ArrayUtil.isEmpty(new long[10]));
+    }
+
+    /**
+     * Test of isEmpty method, of class ArrayUtil.
+     */
+    public void testIsEmptyDoubleArray()
+    {
+        assertTrue(ArrayUtil.isEmpty((double[]) null));
+        assertTrue(ArrayUtil.isEmpty(new double[0]));
+        assertFalse(ArrayUtil.isEmpty(new double[1]));
+        assertFalse(ArrayUtil.isEmpty(new double[2]));
+        assertFalse(ArrayUtil.isEmpty(new double[10]));
+    }
+
+
+    /**
+     * Test of isEmpty method, of class ArrayUtil.
+     */
+    public void testIsEmptyObjectArray()
+    {
+        assertTrue(ArrayUtil.isEmpty((Object[]) null));
+        assertTrue(ArrayUtil.isEmpty(new Object[0]));
+        assertFalse(ArrayUtil.isEmpty(new Object[1]));
+        assertFalse(ArrayUtil.isEmpty(new Object[2]));
+        assertFalse(ArrayUtil.isEmpty(new Object[10]));
     }
 
 }
