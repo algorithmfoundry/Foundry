3926325e72889ea0e7c3a7b52ac905199f375372
jbasilico <jbasilico@algorithmfoundry.org>
2016-10-20 20:39:40 -0700

    Added package info for custom matrix package.

---
+++ Components/CommonCore/Source/gov/sandia/cognition/math/matrix/custom/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * File:                package-info.java
+ * Authors:             Justin Basilico
+ * Company:             Sandia National Laboratories
+ * Project:             Cognitive Foundry
+ * 
+ * Copyright 2015, Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive 
+ * license for use of this work by or on behalf of the U.S. Government. Export 
+ * of this program may require a license from the United States Government. 
+ * See CopyrightHistory.txt for complete details.
+ */
+
+/**
+ * Provides a custom linear algebra package implementation for both dense
+ * and sparse classes.
+ *
+ * @author Jeremy D. Wendt
+ * @since 3.4.4
+ */
+@gov.sandia.cognition.annotation.Documentation
+package gov.sandia.cognition.math.matrix.custom;
