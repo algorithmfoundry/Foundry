22ffab292dde5486e0ad1535a71f42c096d5bb52
Justin Basilico <jbasilico@netflix.com>
2016-11-17 21:08:05 -0800

    Updated summary statistics interface and classes.

---
+++ Components/LearningCore/Test/gov/sandia/cognition/learning/test/PropertyChecker.java
@@ -0,0 +1,113 @@
+/*
+ * File:            DefaultSummaryStatisticsTest.java
+ * Authors:         Justin Basilico
+ * Project:         Cognitive Foundry
+ * 
+ * Copyright 2016 Cognitive Foundry. All rights reserved.
+ */
+
+package gov.sandia.cognition.learning.test;
+
+import java.util.function.ObjDoubleConsumer;
+import java.util.function.ToDoubleFunction;
+import static org.junit.Assert.*;
+
+/**
+ * Utility class for asserting aspects of bean properties.
+ * 
+ * @author Justin Basilico
+ * @since 3.4.4
+ */
+public class PropertyChecker
+    extends Object
+{
+    /**
+     * Protected to prevent creation of singleton class.
+     */
+    protected PropertyChecker()
+    {
+        super();
+    }
+    
+    /**
+     * Checks that the getter/setter of a double property works as expected.
+     *
+     * @param   <T>
+     *      The type of the instance.
+     * @param   instance
+     *      The instance to check the getter/setter on.
+     * @param   name
+     *      The name of the property to check.
+     * @param   getter
+     *      The getter for the property.
+     * @param   setter
+     *      The setter for the property.
+     * @param   expectedInitialValue
+     *      The expected initial value from the setter.
+     * @param   goodValues
+     *      The array of good values to check. They should work when calling
+     *      the getter/setter.
+     * @param   badValues
+     *      The array of bad values to check. They should thrown an
+     *      {@link IllegalArgumentException} in the setter and the value should
+     *      remain unchanged.
+     */
+    public static <T> void checkGetSetDouble(
+        final T instance,
+        final String name,
+        final ToDoubleFunction<T> getter,
+        final ObjDoubleConsumer<T> setter,
+        final double expectedInitialValue,
+        final double[] goodValues,
+        final double[] badValues)
+    {
+        try
+        {
+            // Make sure the getter works.
+            double value = expectedInitialValue;
+            assertEquals(value, getter.applyAsDouble(instance), 0.0);
+
+            // Now make sure the setter works by setting the initial value
+            // again.
+            setter.accept(instance, expectedInitialValue);
+            assertEquals(value, getter.applyAsDouble(instance), 0.0);
+
+            for (final double goodValue : goodValues)
+            {
+                value = goodValue;
+                setter.accept(instance, value);
+                assertEquals(value, getter.applyAsDouble(instance), 0.0);
+            }
+
+            if (badValues != null)
+            {
+                for (final double badValue : badValues)
+                {
+                    boolean exceptionThrown = false;
+                    try
+                    {
+                        setter.accept(instance, badValue);
+                    }
+                    catch (Exception e)
+                    {
+                        exceptionThrown = e instanceof IllegalArgumentException
+                            || e.getCause() instanceof IllegalArgumentException;
+                    }
+                    finally
+                    {
+                        assertTrue("Expected an IllegalArgumentException when "
+                            + "calling setter for" + name + " with value " 
+                            + badValue + " but got none.",
+                            exceptionThrown);
+                    }
+
+                    assertEquals(value, getter.applyAsDouble(instance), 0.0);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Components/LearningCore/Test/gov/sandia/cognition/statistics/DefaultSummaryStatisticsTest.java
@@ -0,0 +1,235 @@
+/*
+ * File:            DefaultSummaryStatisticsTest.java
+ * Authors:         Justin Basilico
+ * Project:         Cognitive Foundry
+ * 
+ * Copyright 2016 Cognitive Foundry. All rights reserved.
+ */
+
+package gov.sandia.cognition.statistics;
+
+import gov.sandia.cognition.learning.test.PropertyChecker;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for class {@link DefaultSummaryStatistics}.
+ * 
+ * @author  Justin Basilico
+ * @since   3.4.4
+ */
+public class DefaultSummaryStatisticsTest
+    extends Object
+{
+    
+    /** An array of doubles for testing. */
+    public static double[] TEST_DOUBLES =
+    {
+        0.0, 1.0, -1.0, 0.1, -0.1, 2.3, -2.3,
+        Double.MIN_VALUE, Double.MAX_VALUE,
+        -Double.MIN_VALUE, -Double.MAX_VALUE,
+        Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY,
+        Double.NaN
+    };
+
+    /** An array of some non-negative doubles. */
+    public static double[] NON_NEGATIVE_DOUBLES =
+    {
+        0.0, 1.0, 0.1, 2.3,
+        Double.MIN_VALUE, Double.MAX_VALUE, Double.MIN_NORMAL,
+        Double.POSITIVE_INFINITY
+    };
+
+    /** An array of some negative doubles and NaN. */
+    public static double[] NEGATIVE_AND_NAN_DOUBLES =
+    {
+        -1.0, -0.1, -2.3,
+        -Double.MIN_VALUE, -Double.MAX_VALUE, -Double.MIN_NORMAL,
+        Double.NEGATIVE_INFINITY,
+        Double.NaN
+    };
+
+    /**
+     * Creates a new test.
+     */
+    public DefaultSummaryStatisticsTest()
+    {
+        super();
+    }
+
+    @Test
+    public void testConstructors()
+    {
+        double mean = 0.0;
+        double variance = 0.0;
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(mean, instance.getMean(), 0.0);
+        assertEquals(variance, instance.getVariance(), 0.0);
+        assertEquals(Math.sqrt(variance), instance.getStandardDeviation(), 0.0);
+        assertEquals(min, instance.getMin(), 0.0);
+        assertEquals(max, instance.getMax(), 0.0);
+
+        mean = 43.21;
+        variance = 98.7;
+        min = 3.2;
+        max = 100.1;
+        instance = new DefaultSummaryStatistics(mean, variance, min, max);
+        assertEquals(mean, instance.getMean(), 0.0);
+        assertEquals(variance, instance.getVariance(), 0.0);
+        assertEquals(Math.sqrt(variance), instance.getStandardDeviation(), 0.0);
+        assertEquals(min, instance.getMin(), 0.0);
+        assertEquals(max, instance.getMax(), 0.0);
+
+        // Make sure that setting a negative variance causes an exception.
+        boolean exceptionThrown = false;
+        try
+        {
+            instance = new DefaultSummaryStatistics(mean, -1.0, min, max);
+        }
+        catch (IllegalArgumentException e)
+        {
+            exceptionThrown = true;
+        }
+        finally
+        {
+            assertTrue(exceptionThrown);
+        }
+    }
+
+    @Test
+    public void testClone()
+    {
+        double mean = 43.21;
+        double variance = 98.7;
+        double min = 3.2;
+        double max = 100.1;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics(mean, variance, min, max);
+        SummaryStatistics clone = instance.clone();
+        assertNotNull(clone);
+        assertNotSame(clone, instance);
+        assertNotSame(clone, instance.clone());
+
+        assertEquals(mean, instance.getMean(), 0.0);
+        assertEquals(variance, instance.getVariance(), 0.0);
+        assertEquals(Math.sqrt(variance), instance.getStandardDeviation(), 0.0);
+        assertEquals(min, instance.getMin(), 0.0);
+        assertEquals(max, instance.getMax(), 0.0);
+
+        assertEquals(mean, clone.getMean(), 0.0);
+        assertEquals(variance, clone.getVariance(), 0.0);
+        assertEquals(Math.sqrt(variance), clone.getStandardDeviation(), 0.0);
+        assertEquals(min, clone.getMin(), 0.0);
+        assertEquals(max, clone.getMax(), 0.0);
+    }
+
+    @Test
+    public void testGetMean()
+    {
+        this.testSetMean();
+    }
+
+    @Test
+    public void testSetMean()
+    {
+        double mean = 0.0;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(mean, instance.getMean(), 0.0);
+
+        instance.setMean(mean);
+        assertEquals(mean, instance.getMean(), 0.0);
+
+        PropertyChecker.checkGetSetDouble(instance, "mean", DefaultSummaryStatistics::getMean,
+            DefaultSummaryStatistics::setMean, mean, TEST_DOUBLES, null);
+    }
+
+    @Test
+    public void testGetVariance()
+    {
+        this.testSetVariance();
+    }
+
+    @Test
+    public void testSetVariance()
+    {
+        double variance = 0.0;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(variance, instance.getVariance(), 0.0);
+
+        instance.setVariance(variance);
+        assertEquals(variance, instance.getVariance(), 0.0);
+
+        PropertyChecker.checkGetSetDouble(instance,
+            "variance", DefaultSummaryStatistics::getVariance,
+            DefaultSummaryStatistics::setVariance, variance,
+            NON_NEGATIVE_DOUBLES, NEGATIVE_AND_NAN_DOUBLES);
+    }
+
+
+    @Test
+    public void testGetStandardDeviation()
+    {
+        this.testSetStandardDeviation();
+    }
+
+    @Test
+    public void testSetStandardDeviation()
+    {
+        double standardDeviation = 0.0;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(standardDeviation, instance.getStandardDeviation(), 0.0);
+
+        instance.setStandardDeviation(standardDeviation);
+        assertEquals(standardDeviation, instance.getStandardDeviation(), 0.0);
+
+        PropertyChecker.checkGetSetDouble(instance, "standardDeviation", 
+            DefaultSummaryStatistics::getStandardDeviation,
+            DefaultSummaryStatistics::setStandardDeviation,
+            standardDeviation, NON_NEGATIVE_DOUBLES, NEGATIVE_AND_NAN_DOUBLES);
+    }
+
+    @Test
+    public void testGetMin()
+    {
+        this.testSetMin();
+    }
+
+    @Test
+    public void testSetMin()
+    {
+        double min = Double.POSITIVE_INFINITY;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(min, instance.getMin(), 0.0);
+
+        instance.setMin(min);
+        assertEquals(min, instance.getMin(), 0.0);
+
+        PropertyChecker.checkGetSetDouble(instance, "min", DefaultSummaryStatistics::getMin,
+            DefaultSummaryStatistics::setMin, min, TEST_DOUBLES, null);
+    }
+
+    @Test
+    public void testGetMax()
+    {
+        this.testSetMax();
+    }
+
+    @Test
+    public void testSetMax()
+    {
+        double max = Double.NEGATIVE_INFINITY;
+        DefaultSummaryStatistics instance = new DefaultSummaryStatistics();
+        assertEquals(max, instance.getMax(), 0.0);
+
+        instance.setMax(max);
+        assertEquals(max, instance.getMax(), 0.0);
+
+        PropertyChecker.checkGetSetDouble(instance, "max", DefaultSummaryStatistics::getMax,
+            DefaultSummaryStatistics::setMax, max,  TEST_DOUBLES, null);
+    }
+    
+// TODO: Put this in a more general place.
+    
+
+}
